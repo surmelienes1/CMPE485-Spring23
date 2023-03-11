823b166cee884c51e628d7f829f4ecb4c924f359
surmelienes1 <74325266+surmelienes1@users.noreply.github.com>
2023-03-11 12:13:49 +0300

    Updated 2.6, Completed 2.7 Adding Textures Step
    Updated 2.6 part. Completed the 2.7 Adding Textures Step defined as "Materials should be created using images, which can then be assigned to the Mesh Renderer of any 3D object. The image can be added to any of the fields of the material (Albedo, Metallic, etc.) proving different results". Used both albedo and metallic options to see the effects of textures on the objects.

---
+++ HW#1/Assets/Scripts/AddForceUpperBackLeft.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddForceUpperBackLeft : MonoBehaviour
+{
+    // Reference to the Rigidbody component
+    public Rigidbody rb;
+
+    // Amount of force to apply to the Rigidbody
+    public float forceAmount = 3f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Apply a force to move the object to the upper back
+        rb.AddForce(new Vector3(-forceAmount, forceAmount, -forceAmount));
+    }
+}
+
